8b4dfba05ee7c8be3a70ce33dc36e1024f926e69
Sunqi <sunqi@miliantech.com>
2021-03-15 18:09:59 +0800

    快排bug修复

--- 01排序算法/quick_sort.go
+++ 01排序算法/quick_sort.go
@@ -3,24 +3,27 @@ package main
 import (
 	"fmt"
 	"leet_code_go/helper"
+	"sort"
 )
 
-func QuickSort(array []int) []int {
-	partition(array, 0, len(array)-1)
+func QuickSort(array []int, left, right int) []int {
+	if left >= right {
+		return array
+	}
+	pivot := partition(array, left, right)
+	QuickSort(array, left, pivot-1)
+	QuickSort(array, pivot+1, right)
 	return array
 }
 
-func partition(array []int, left, right int) {
-	if left >= right {
-		return
-	}
-	pivot := right
+func partition(array []int, left, right int) int {
+	pivot := array[right]
 	var l, r = left, right - 1
-	for l < r {
-		for l < pivot && array[l] <= array[pivot] {
+	for l <= r {
+		for l <= r && array[l] <= pivot {
 			l++
 		}
-		for r > left && array[r] >= array[pivot] {
+		for l <= r && array[r] > pivot {
 			r--
 		}
 
@@ -28,13 +31,22 @@ func partition(array []int, left, right int) {
 			array[l], array[r] = array[r], array[l]
 		}
 	}
-	array[l], array[pivot] = array[pivot], array[l]
-	partition(array, left, l-1)
-	partition(array, l+1, right)
+	array[l], array[right] = array[right], array[l]
+	return l
 }
 func main() {
-	array := helper.RandomIntArray(10, 1000)
-	fmt.Println(array)
-	result := QuickSort(array)
+	array := helper.RandomIntArray(1000, 1000)
+	newArray := make([]int, len(array))
+	for index, value := range array {
+		newArray[index] = value
+	}
+	result := QuickSort(newArray, 0, len(newArray)-1)
+	sort.Ints(array)
 	fmt.Println(result)
+	for index, value := range array {
+		if value != result[index] {
+			fmt.Println(index)
+			panic("结果不一样")
+		}
+	}
 }
