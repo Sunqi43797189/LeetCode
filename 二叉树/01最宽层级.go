42c7f814ae94ff9fb86d3423e494f1573a3542d7
Sunqi <sunqi@miliantech.com>
2021-04-13 12:08:02 +0800

    添加二叉树宽度非map

--- 二叉树/01最宽层级.go
+++ 二叉树/01最宽层级.go
@@ -6,7 +6,7 @@ import (
 	"math"
 )
 
-func BinaryTreeMaxCountLevel(node *helper.BinaryTreeNode) (level int, count int) {
+func BinaryTreeMaxCountLevelWithMap(node *helper.BinaryTreeNode) (level int, count int) {
 	if node == nil {
 		return
 	}
@@ -15,7 +15,7 @@ func BinaryTreeMaxCountLevel(node *helper.BinaryTreeNode) (level int, count int)
 		node: 1,
 	}
 	curLevel, curLevelNodes, maxCount := 1, 0, -1
-	for len(queue) != 0 {
+	for ; len(queue) != 0; {
 		node := queue[0]
 		queue = queue[1:]
 		if curLevel == nodeMap[node] {
@@ -38,7 +38,38 @@ func BinaryTreeMaxCountLevel(node *helper.BinaryTreeNode) (level int, count int)
 	return curLevel - 1, maxCount
 }
 
+func BinaryTreeMaxCountLevel(root *helper.BinaryTreeNode) (count int) {
+	if root == nil {
+		return
+	}
+	queue := []*helper.BinaryTreeNode{root}
+	var curEnd, nextEnd *helper.BinaryTreeNode
+	curEnd, nextEnd, curLevelNodes, maxCount := root, nil, 0, -1
+	for ; len(queue) != 0; {
+		node := queue[0]
+		queue = queue[1:]
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+			nextEnd = node.Left
+		}
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+			nextEnd = node.Right
+		}
+		if curEnd == node {
+			curEnd = nextEnd
+			maxCount = int(math.Max(float64(maxCount), float64(curLevelNodes)))
+			curLevelNodes = 1
+		} else {
+			curLevelNodes++
+		}
+
+	}
+	return maxCount
+}
+
 func main() {
 	root := helper.FullBinaryTree()
+	fmt.Println(BinaryTreeMaxCountLevelWithMap(root))
 	fmt.Println(BinaryTreeMaxCountLevel(root))
 }
