b75fd4610db5190086882281af0cbb017eb30365
sunqi <15735657423@163.com>
2021-04-20 21:25:39 +0800

    debug

--- dfs/03电话号码的字母组合.go
+++ dfs/03电话号码的字母组合.go
@@ -5,29 +5,28 @@ import (
 	"strings"
 )
 
-var data = [][]string{
-	{},
-	{},
-	{"a", "b", "c"},
-	{"d", "e", "f"},
-	{"g", "h", "i"},
-	{"j", "k", "l"},
-	{"m", "n", "o"},
-	{"p", "q", "r", "s"},
-	{"t", "u", "v"},
-	{"w", "x", "y", "z"},
+var data = map[string][]string{
+	"0": {},
+	"1": {},
+	"2": {"a", "b", "c"},
+	"3": {"d", "e", "f"},
+	"4": {"g", "h", "i"},
+	"5": {"j", "k", "l"},
+	"6": {"m", "n", "o"},
+	"7": {"p", "q", "r", "s"},
+	"8": {"t", "u", "v"},
+	"9": {"w", "x", "y", "z"},
 }
 
 var res = []string{}
 
-func DfsPhoneNum(str string, index int, res []string, tempArr []string) {
+func DfsPhoneNum(str string, index int, res *[]string, tempArr []string) {
+	array := strings.Split(str, "")
 	if index == len(str) {
-		res = append(res, strings.Join(tempArr, ""))
-		fmt.Println(res)
+		*res = append(*res, strings.Join(tempArr, ""))
 		return
 	}
-	for _, value := range data[index] {
-		fmt.Println(value)
+	for _, value := range data[array[index]] {
 		tempArr = append(tempArr, value)
 		DfsPhoneNum(str, index+1, res, tempArr)
 		tempArr = tempArr[:len(tempArr)-1]
@@ -36,8 +35,8 @@ func DfsPhoneNum(str string, index int, res []string, tempArr []string) {
 }
 
 func main() {
-	str := "2,3"
+	str := "239"
 	temp := []string{}
-	DfsPhoneNum(str, 0, res, temp)
+	DfsPhoneNum(str, 0, &res, temp)
 	fmt.Println(res)
 }
