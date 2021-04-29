e059fcb8c154937c733420f7b326f08345ead847
Sunqi <sunqi@miliantech.com>
2021-04-29 14:54:05 +0800

    埃氏筛选法

--- leetcode/0素数.go
+++ leetcode/0素数.go
@@ -1,5 +1,6 @@
 package leetcode
 
+// 暴力算法
 func PrimeCount(n int) int {
 	if n <= 1 {
 		return 0
@@ -8,6 +9,24 @@ func PrimeCount(n int) int {
 	for x := 2; x < n; x++ {
 		if isPrime(x) {
 			count += 1
+		}
+	}
+	return count
+}
+
+// 埃氏筛选法
+func Eratosthenes(n int) int {
+	if n <= 1 {
+		return 0
+	}
+	arr := make([]bool, n) // 初始默认所有都为素数
+	count := 0
+	for i := 2; i < n; i++ {
+		if arr[i] == false {
+			count++
+			for j := i * i; j < n; j += i {
+				arr[j] = true
+			}
 		}
 	}
 	return count
--- leetcode/0素数_test.go
+++ leetcode/0素数_test.go
@@ -9,3 +9,8 @@ func TestPrimeCount(t *testing.T) {
 	n := 100
 	fmt.Println(PrimeCount(n))
 }
+
+func TestEratosthenes(t *testing.T) {
+	n := 100
+	fmt.Println(Eratosthenes(n))
+}
